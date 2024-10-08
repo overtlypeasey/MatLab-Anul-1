c1d169cc3f4a2d10d7a7fa7801a4075e440e7a2d
Victor <victor.hasieganu@gmail.com>
2024-10-08 15:54:23 +0300

    Lab 8/10

--- Lab_1.m
+++ Lab_1.m
@@ -1,3 +1,18 @@
 a = 2;
 b = 3;
-c = a+b
+c = a+b;
+
+matrix1 = [1 2 3; 4 5 6; 7 8 9];
+inv(matrix1);
+matrix1';
+
+sqrt(-1);
+complex = [1+i, 2+j, 4-j];
+
+matrix1(1, 1);
+matrix1(3, 2);
+matrix1(1, 1) + matrix1(3, 2);
+
+A=magic(4)
+B=rand(4, 3, 2)
+
