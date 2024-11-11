7dc9fca9fa2ee8ad2d0fdd516eee53facc25b2f1
Victor <victor.hasieganu@gmail.com>
2024-11-11 18:05:02 +0200

    12/11/24 Assignment

--- Assignemnt5.m
+++ Assignemnt5.m
@@ -2,6 +2,27 @@ clc
 clear
 close all
 
-c = linspace(-2-2.*i, 2+2.*i);
+x = linspace(-2, 2, 600);
+y = linspace(-2, 2, 600);
+[X, Y] = meshgrid(x, y);
+C = X + i * Y;
 
-for z = 1:600
+% Initialize the iteration count matrix
+maxIter = 50;
+Z = zeros(size(C));
+M = zeros(size(C));
+
+% Mandelbrot iteration
+for k = 1:maxIter
+    Z = Z.^2 + C;
+    M(abs(Z) <= 1000) = k;
+end
+
+% Plot the Mandelbrot set
+figure
+image(x, y, M)
+colormap([jet(); flipud(jet()); 0 0 0]);
+colorbar
+axis off;
+title('Mandelbrot Set');
+
