c0927c06477892b2e4512e8eb770ae273e990661
Victor <victor.hasieganu@gmail.com>
2025-01-13 18:29:00 +0200

    Falcon 9 is a partially reusable, human-rated, two-stage-to-orbit, medium-lift launch vehicle designed and manufactured in the United States by SpaceX

--- MandatoryAsssignment4.m
+++ MandatoryAsssignment4.m
@@ -1,27 +1,22 @@
-% Number of points
 N = 500;
 
-% Generate random X and Y coordinates between -10 and 10
 X = -10 + (10 - (-10)) * rand(N, 1);
 Y = -10 + (10 - (-10)) * rand(N, 1);
 
 distances = sqrt(X.^2 + Y.^2);
 
-% Filter points inside the circle of radius 10
 inside_circle = distances <= 10;
 X = X(inside_circle);
 Y = Y(inside_circle);
 
 distances = sqrt(X.^2 + Y.^2);
 
-% Filter points outside the circle of radius 7
 outside_inner_circle = distances >= 7;
 X = X(outside_inner_circle);
 Y = Y(outside_inner_circle);
 
 K = convhull(X, Y);
 
-% Plot the points and the path connecting them
 figure;
 scatter(X, Y, 'filled');
 hold on;
